ce5475def333793b79a317037a53637e47610e9c
unknown <eparslow18@LAB-SCI-304-14.whitworth.edu>
2016-04-06 21:31:59 -0700

    Writing Java for Login Page

--- TrackMyTrack2/app/src/main/java/trackmytrack/azurewebsites/net/Login.java
+++ TrackMyTrack2/app/src/main/java/trackmytrack/azurewebsites/net/Login.java
@@ -4,34 +4,74 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class Login extends AppCompatActivity{
+//declare variables
+    Button loginbutton;
+    Button signupbutton;
+    String emailtxt;
+    String passwordtxt; //text entered into password and email
+    EditText ETemail; //text linked to id
+    EditText ETpassword;//text linked to id
+
+    TextView tx1; //check credetials and times been entered
+    int counter =3;
+
+        //call when the activity is first created
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+        loginbutton=(Button)findViewById(R.id.loginbutton);
+        ETemail=(EditText)findViewById(R.id.ETemail);
+        ETpassword=(EditText)findViewById(R.id.ETpassword);
+        signupbutton=(Button)findViewById(R.id.SignupButton);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Login Button Click Listener
+        loginbutton.setOnClickListener(new OnClickListener() {//clicking if credentials are filled
+            @Override
+            public void onClick(View v) {
+                // Retrieve the text entered from the EditText
+                if (ETemail.getText().toString().equals("admin") &&
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                        ETpassword.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
 
-        return super.onOptionsItemSelected(item);
+                    tx1.setVisibility(View.VISIBLE);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        loginbutton.setEnabled(false);
+                    }
+                    //emailtxt = ETemail.getText().toString();
+                    //passwordtxt = ETpassword.getText().toString();
+
+                }
+            }
+        });
+        // Sign up Button Click Listener //http://www.tutorialspoint.com/android/android_login_screen.htm
+        //https://docs.oracle.com/javase/specs/jls/se7/html/
+        signupbutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve the text entered from the EditText
+                emailtxt = ETemail.getText().toString();
+                passwordtxt = ETpassword.getText().toString();
+
+
+
+            }
+        });
+
     }
 }
